c64d09c45f89774719399cf936bd236f234087cb
ayomiga <oluwayemiayo@yahoo.com>
2017-09-18 09:39:29 +0100

    Running database migrations ORM

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -26,3 +26,15 @@ from django.conf.urls import include
 urlpatterns += [
     url(r'^catalog/', include('catalog.urls')),
 ]
+
+#redirection to the app catalog
+#Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+urlpatterns += [
+    url(r'^$', RedirectView.as_view(url='/catalog/', permanent=True)),
+]
+#Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
