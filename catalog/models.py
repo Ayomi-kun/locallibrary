021c34a8335d847214357a13af9bf305425e4ddb
ayomiga <oluwayemiayo@yahoo.com>
2017-09-19 11:32:39 +0100

    bookInstances

--- catalog/models.py
+++ catalog/models.py
@@ -3,17 +3,15 @@ from django.db import models
 
 class Genre(models.Model):
     """
-    Model representating a book genre (e.g. Science Fiction, Non Fiction).
+    Model representing a book genre (e.g. Science Fiction, Non Fiction).
     """
-
-    name = models.CharField(max_length=200, help_text="Enter a book genre (e.g. Science Fiction, French Peotry etc.)")
-
+    name = models.CharField(max_length=200, help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
+    
     def __str__(self):
         """
-        String for  representing the Model object (in Admin site etc.)
+        String for representing the Model object (in Admin site etc.)
         """
         return self.name
-        
 
 from django.urls import reverse #Used to generate URLs by reversing the URL patterns
 
@@ -43,3 +41,33 @@ class Book(models.Model):
         Returns the url to access a particular book instance.
         """
         return reverse('book-detail', args=[str(self.id)])
+
+import uuid # Required for unique book instances
+
+class BookInstance(models.Model):
+    """
+    Model representing a specific copy of a book (i.e. that can be borrowed from the library).
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular book across whole library")
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True) 
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='Book availability')
+
+    class Meta:
+        ordering = ["due_back"]
+        
+
+    def __str__(self):
+        """
+        String for representing the Model object
+        """
+        return '%s (%s)' % (self.id,self.book.title)
